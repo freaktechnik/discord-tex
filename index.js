593f4e3ef73855c223c3887dedee769b3e2a2c85
Martin Giger <martin@humanoids.be>
2017-04-12 21:19:45 +0200

    fix index

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-< %ûØ ‹¡p8‚ÍPM÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4
- 6WïsáÍª¶-eëÐcÿú•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M÷ÚØ•nÉî}ïA½4M
+const Eris = require("eris");
+const mj = require("mathjax-node");
+
+const bot = new Eris(process.env.DISCORD_TOKEN);
+mj.config({
+    displayMessages: false,
+    displayErrors: false,
+    undefinedCharError: false
+});
+mj.start();
+
+bot.on("messageCreate", (msg) => {
+    if(msg.content.startsWith("TeX:")) {
+        const math = msg.content.substr(5);
+        mj.typeset({
+            math,
+            mml: true
+        }, (data) => {
+            if(!data.errors) {
+                bot.createMessage(msg.channel.id, {
+                    content: "",
+                    embed: {
+                        type: "rich",
+                        title: math,
+                        description: data.mml
+                    }
+                });
+            }
+            else {
+                bot.createMessage(msg.channel.id, {
+                    content: `Errors occured while parsing the TeX expression "${math}"`,
+                    embed: {
+                        title: "Errors",
+                        color: "#ff0000",
+                        fields: data.erros.map((e) => {
+                            return {
+                                name: "error",
+                                value: e
+                            };
+                        })
+                    }
+                });
+            }
+        })
+    }
+})
